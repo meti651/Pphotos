3660b7895fe6e76f3209a3e00515930988e90c10
meti651 <meti651@gmail.com>
2020-12-01 13:22:38 +0100

    import google fonts to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,16 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    "gatsby-plugin-sass"
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `merriweather`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
