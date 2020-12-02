aa68de0ecda5b488634ed4751853c165d14556fd
meti651 <meti651@gmail.com>
2020-12-02 15:56:27 +0100

    add about-me part to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import AboutMe from "../components/about-me";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <AboutMe/>
       <Footer/>
     </>
   )
