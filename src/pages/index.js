1db9df7161d91157edc26c14a070e89d9d7842bf
meti651 <meti651@gmail.com>
2020-12-04 15:51:49 +0100

    quotes component added to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 import AboutMe from "../components/about-me";
 import Slider from "../components/slider/photo-slider";
+import Quotes from "../components/slider/quotes";
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Slider/>
+      <Quotes/>
       <AboutMe/>
       <Footer/>
     </>
