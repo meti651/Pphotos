b9fe9da5576a8848fdc32901fd868b0b2b46483c
meti651 <meti651@gmail.com>
2020-12-04 14:33:29 +0100

    slider to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import AboutMe from "../components/about-me";
+import Slider from "../components/slider/photo-slider";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Slider/>
       <AboutMe/>
       <Footer/>
     </>
