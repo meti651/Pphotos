36a8d416e00677d9cb08d077e43bc072e936a367
meti651 <meti651@gmail.com>
2020-12-08 11:56:29 +0100

    parallax implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,38 @@
-import React from "react";
-import Footer from "../components/footer";
-import Header from "../components/header";
-import AboutMe from "../components/about-me";
-import Slider from "../components/slider/photo-slider";
-import Quotes from "../components/slider/quotes";
-import Opinions from "../components/opinions";
+import React, {useState, useRef} from "react";
+import Layout from "../components/layout";
+import AboutMe from "../components/landing-page/about-me";
+import Slider from "../components/landing-page/slider/photo-slider";
+import Quotes from "../components/landing-page/slider/quotes";
+import Opinions from "../components/landing-page/opinions";
+import NavigationBoxes from "../components/landing-page/navigationBoxes";
+import Charity from "../components/landing-page/charity";
+
+import "./index.scss";
 
 export default function Home() {
+  const [onTop, setOnTop] = useState(true);
+  const scrollDisplay = useRef();
+
+  const handleScroll = () => {
+    if(scrollDisplay.current.scrollTop === 0) setOnTop(true);
+    else setOnTop(false);
+  }
+
   return (
     <>
-      <Header/>
-      <Slider/>
-      <Quotes/>
-      <AboutMe/>
-      <Opinions/>
-      <Footer/>
+        <Layout onTop={onTop}>
+      <main className="main" onScroll={handleScroll} ref={scrollDisplay}>
+          <Slider/>
+          <Quotes/>
+          <section className="section parallax parallax-one"></section>
+          <AboutMe/>
+          <section className="section parallax parallax-two"></section>
+          <Opinions/>
+          <section className="section parallax parallax-three"></section>
+          <NavigationBoxes/>
+          <Charity/>
+      </main>
+        </Layout>
     </>
   )
 }
