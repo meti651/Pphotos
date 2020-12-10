6f11f20adc3db38303bc410b5e94a6d1fa17df74
meti651 <meti651@gmail.com>
2020-12-10 10:20:25 +0100

    subpages slider implemented

--- src/components/landing-page/slider/arrow.js
+++ src/components/landing-page/slider/arrow.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 import Styles from "./arrow.module.scss";
 
-const Arrow = ({direction}) => {
+const Arrow = ({direction, translate, setTranslate}) => {
     const [arrow, setArrow] = useState("");
 
     useEffect(() => {
@@ -10,8 +10,16 @@ const Arrow = ({direction}) => {
         else setArrow(">");
     }, [])
 
+    const handleClick = () => {
+        let resultTranslate = translate;
+        if(direction === "left") resultTranslate--;
+        else resultTranslate++;
+        if(resultTranslate > 3 || resultTranslate < 0) resultTranslate = 0;
+        setTranslate(resultTranslate);
+    }
+
     return (
-        <div className={Styles.container} id={direction === "right" ? Styles.right : undefined}>
+        <div className={Styles.container} id={direction === "right" ? Styles.right : undefined} onClick={handleClick}>
             {arrow}
         </div>
     )
--- src/components/subpages/slider.js
+++ src/components/subpages/slider.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Arrow from "../landing-page/slider/arrow";
 
 import Styles from "./slider.module.scss";
 
 const Slider = ({images}) => {
+    const [translate, setTranslate] = useState(0);
+
     return (
         <div id={Styles.container}>
-            <Arrow direction="left"/>
+            <Arrow direction="left" translate={translate} setTranslate={setTranslate}/>
             <div id={Styles.images_container}>
-                {images.map((image,index) => <img key={index} src={image} alt={index}/>)}
+                {images.map((image,index) => <img style={{transform: `translateX(-${30 * translate}vw)`}} key={index} src={image} alt={index}/>)}
             </div>
-            <Arrow direction="right" />
+            <Arrow direction="right" translate={translate} setTranslate={setTranslate}/>
         </div>
     )
 }
