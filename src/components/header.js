117a99931995153c1a51fc86bb9b564771267625
meti651 <meti651@gmail.com>
2020-12-10 12:33:44 +0100

    link path modification

--- src/components/header.js
+++ src/components/header.js
@@ -45,7 +45,7 @@ const Header = ({onTop}) => {
                         <Link to="/szolgaltatasok/gyerek-fotozas">Gyerek fotózás</Link> 
                         <Link to="/szolgaltatasok/kismama-fotozas">Kismama fotózás</Link> 
                         <Link to="/szolgaltatasok/paros-fotozas">Páros fotózás</Link> 
-                        <Link to="/szolgaltatasok/portré-fotozas">Portré fotózás</Link>
+                        <Link to="/szolgaltatasok/portre-fotozas">Portré fotózás</Link>
                     </div>   
                 </div>
                 <Link to="/galeria">Galéria</Link>
