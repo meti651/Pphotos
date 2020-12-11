d7d618733dc23daea8b2ec5963a0c8f0ff1cd910
meti651 <meti651@gmail.com>
2020-12-11 01:41:19 +0100

    prepare image component for the family page

---
+++ src/components/subpages/family/image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Styles from "./image.module.scss";
+
+import MiddleImage from "../../../images/family-middle.png";
+
+const Image = () => {
+    return <div id={Styles.middle_image} style={{backgroundImage: `url(${MiddleImage})`}}></div>
+}
+
+export default Image;
