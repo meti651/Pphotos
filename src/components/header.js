e8e542c012dab6af40f9f9f2c21f980e8400bac4
meti651 <meti651@gmail.com>
2020-12-16 11:44:14 +0100

    new header style

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,6 @@ import Styles from "./header.module.scss";
 
 const Header = ({onTop}) => {
     const [open, setOpen] = useState(false);
-    const [isHovered, setIsHovered] = useState(false);
-    const [isDisplayed, setIsDisplayed] = useState(false);
-
-    useEffect(() => {
-        if(!onTop || isHovered) setIsDisplayed(true);
-        else setIsDisplayed(false);
-    }, [onTop, isHovered])
 
     const handleHover = (_) => {
         setOpen(true);
@@ -22,36 +15,35 @@ const Header = ({onTop}) => {
         setOpen(false);
     }
 
-    const handleHeaderHovering = () => {
-        setIsHovered(true);
-    }
-
-    const handleHeaderBlur = () => {
-        setIsHovered(false);
-    }
-
     return (
-        <header style={{"opacity": isDisplayed ? "100%" : "20%"}} onMouseEnter={handleHeaderHovering} onMouseLeave={handleHeaderBlur}>
-            <div className={Styles.image} >
-                <img src={Logo} alt="Logo" />
-            </div>
+        <header className={onTop ? Styles.on_top : undefined}>
             <div id={Styles.links} >
-                <Link to="/">Kezdőlap</Link>
-                <div onMouseEnter={handleHover} onMouseLeave={handleBlur} id={Styles.dropdown}>
-                    <p>Árak/Szolgáltatások</p>
-                    <div id={Styles.dropdownMenu} className={open ? Styles.open : undefined}>
-                        <Link to="/szolgaltatasok/csaladi-fotozas">Családi fotózás</Link> 
-                        <Link to="/szolgaltatasok/eskuvoi-fotozas">Esküvői fotózás</Link> 
-                        <Link to="/szolgaltatasok/gyerek-fotozas">Gyerek fotózás</Link> 
-                        <Link to="/szolgaltatasok/kismama-fotozas">Kismama fotózás</Link> 
-                        <Link to="/szolgaltatasok/paros-fotozas">Páros fotózás</Link> 
-                        <Link to="/szolgaltatasok/portre-fotozas">Portré fotózás</Link>
-                    </div>   
+                <div className={Styles.part} id={Styles.left_side}>
+                    <Link to="/">Kezdőlap</Link>
+                    <div onMouseEnter={handleHover} onMouseLeave={handleBlur} id={Styles.dropdown}>
+                        <p>Árak/Szolgáltatások</p>
+                        <div id={Styles.dropdownMenu} className={open ? Styles.open : undefined}>
+                            <Link to="/szolgaltatasok/csaladi-fotozas">Családi fotózás</Link> 
+                            <Link to="/szolgaltatasok/eskuvoi-fotozas">Esküvői fotózás</Link> 
+                            <Link to="/szolgaltatasok/gyerek-fotozas">Gyerek fotózás</Link> 
+                            <Link to="/szolgaltatasok/kismama-fotozas">Kismama fotózás</Link> 
+                            <Link to="/szolgaltatasok/paros-fotozas">Páros fotózás</Link> 
+                            <Link to="/szolgaltatasok/portre-fotozas">Portré fotózás</Link>
+                        </div>   
+                    </div>
                 </div>
-                <Link to="/galeria">Galéria</Link>
-                <Link to="/kapcsolatok">Kapcsolatok</Link>
+                <div className={Styles.image} >
+                    <div id={Styles.presentation} style={{opacity: onTop ? "100%" : "0"}}>
+                        <h1>Baracskai Petra</h1>
+                        <h2>Photography</h2>
+                    </div>
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <div className={Styles.part} id={Styles.right_side}>
+                    <Link to="/galeria">Galéria</Link>
+                    <Link to="/kapcsolatok">Kapcsolatok</Link>
+                </div>
             </div>
-            <div id={Styles.placeholder}></div>
         </header>
     )
 }
