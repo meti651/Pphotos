e5ac7ea56d59183e6b5da51b9883daece2cc4630
meti651 <meti651@gmail.com>
2020-12-18 14:32:47 +0100

    slider v 1.2

--- src/components/landing-page/slider/slide.js
+++ src/components/landing-page/slider/slide.js
@@ -3,8 +3,14 @@ import React from 'react';
 import Styles from "./slide.module.scss";
 
 const Slide = ({image, slide}) => {
+
     return (
-        <div className={Styles.container_wrapper} style={{transform: `translateX(-${100*slide}vw)`, backgroundImage: `url(${image.image})`}}>
+        <div className={Styles.container_wrapper} 
+            style={{
+                transform: `translateX(-${100*slide}vw)`,
+                backgroundImage: `url(${image.image})`
+                }}
+            >
             <div className={Styles.container}>
                 <div id={Styles.caption}>
                     {image.caption}
