26c26f8eb35d56a33bb99f93249c8bac2160ede0
meti651 <meti651@gmail.com>
2020-12-18 15:01:23 +0100

    add gatsby links to the boxes

--- src/components/landing-page/navigationBox.js
+++ src/components/landing-page/navigationBox.js
@@ -1,17 +1,18 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 import Styles from "./navigationBox.module.scss"
 
 const NavigationBox = ({link, image}) => {
     return (
-        <a href={link} id={Styles.container}>
+        <Link href={`/szolgaltatasok${link}`} id={Styles.container}>
             <div id={Styles.image_wrapper}>
                 <img src={image.image} alt={image.title}/>
             </div>
             <div id={Styles.title}>
                 {image.title}
             </div>
-        </a>
+        </Link>
     )
 }
 
