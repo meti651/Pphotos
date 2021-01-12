624f4ab1ec555b932d486e2a51992380ffd3f08e
meti651 <meti651@gmail.com>
2021-01-12 23:10:12 +0100

    delete layout from 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,13 @@ import Layout from '../components/layout';
 
 const pageNotFound = () => {
     return(
-        <Layout>
-            <div>
-                <h1>Page not found</h1>
-                <p>Oops! The page you are looking for is not exist anymore</p>
-                <Link to="/">Go to Home</Link>
-            </div>
-        </Layout>
+    
+        <div>
+            <h1>Page not found</h1>
+            <p>Oops! The page you are looking for is not exist anymore</p>
+            <Link to="/">Go to Home</Link>
+        </div>
+    
     )
 }
 
