37db6b0bab6571210251f9905fba894c5efd72f9
meti651 <meti651@gmail.com>
2021-01-12 12:12:52 +0100

    change landing page slider img tags to gatsby-img component

--- src/components/landing-page/slider/photo-slider.js
+++ src/components/landing-page/slider/photo-slider.js
@@ -1,3 +1,4 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React, {useState, useEffect} from 'react';
 
 import Wedding from "../../../images/slider/1.jpg";
@@ -10,17 +11,84 @@ import Portrait from "../../../images/slider/6.jpg";
 import Styles from "./photo-slider.module.scss";
 import Slide from './slide';
 
-const slidesElements = [
-    {"image": Wedding, "caption": "esküvői", "quote": "A házasság 3 dologról szól: Emlékezni az összetartozásra, elfelejteni egymás hibáit és kitartani egymás mellett mindvégig."}, 
-    {"image": Family, "caption": "családi", "quote": "Néha az ember akkor értékeli igazán a pillanatot mikor már emlékké vált."}, 
-    {"image": Mum, "caption": "kismama", "quote": "Mikor fotózunk, valójában retúrjegyet veszünk a pillanathoz, ami elmúlt."},
-    {"image": Children, "caption": "gyerek", "quote": "Gyermekkor egyet jelent: Egyszerűség. Egy gyermek szemével nézni a világot egyszerűen gyönyörű."},
-    {"image": Couple, "caption": "páros", "quote": "Néha azt kívánom bárcsak visszamehetnék az időbe… Nem változtatnék meg semmit csak újra élnem azt a pillanatot."},
-    {"image": Portrait, "caption": "portré", "quote": "A fénykép valójában a pillanat megállító gombja."}];
+
 
 const Slider = () => {
     
     const [slide, setSlide] = useState(0);
+    // const [slidesElements, setSlidesElements] = useState();
+    const images = useStaticQuery(graphql`
+    {
+        first: 
+            file(relativePath: {eq: "slider/1.jpg"}) {
+              relativePath
+              childImageSharp {
+                fluid(maxWidth: 1600) {
+                  srcSet
+                }
+              }
+            }
+          
+        second: 
+            file(relativePath: {eq: "slider/2.jpg"}) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1600) {
+                    srcSet
+                  }
+                }
+              }
+        
+        third: 
+            file(relativePath: {eq: "slider/3.jpg"}) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1600) {
+                    srcSet
+                  }
+                }
+              }
+        
+        fourth: 
+            file(relativePath: {eq: "slider/4.jpg"}) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1600) {
+                    srcSet
+                  }
+                }
+              }
+        
+        fifth: 
+            file(relativePath: {eq: "slider/5.jpg"}) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1600) {
+                    srcSet
+                  }
+                }
+              }
+        
+        sixth: 
+            file(relativePath: {eq: "slider/6.jpg"}) {
+                relativePath
+                childImageSharp {
+                  fluid(maxWidth: 1600) {
+                    srcSet
+                  }
+                }
+              }
+        
+    }
+      `);
+        console.log(images);
+      const slidesElements = [
+        {"image": images.first, "caption": "esküvői"}, 
+        {"image": images.second, "caption": "családi"}, 
+        {"image": images.third, "caption": "kismama"},
+        {"image": images.fourth, "caption": "gyerek"},
+        {"image": images.fifth, "caption": "páros"},
+        {"image": images.sixth, "caption": "portré"}];
 
     useEffect(() => {
         let slideNum = slide;
@@ -34,6 +102,7 @@ const Slider = () => {
         <div id={Styles.container}>
             {slidesElements.map((image, index) => <Slide key={index} image={image} slide={slide}/>)}
         </div>
+        <p id={Styles.quote}>Élmény alapú fotózás</p>
         </>
     )
 }
--- src/components/landing-page/slider/slide.js
+++ src/components/landing-page/slider/slide.js
@@ -1,29 +1,29 @@
 import React from 'react';
+import Img from "gatsby-image";
 
 import Styles from "./slide.module.scss";
 
 const Slide = ({image, slide}) => {
 
     return (
+        <>
         <div className={Styles.container_wrapper} 
             style={{
-                transform: `translateX(-${100*slide}vw)`,
-                backgroundImage: `url(${image.image})`
+                transform: `translateX(-${100*slide}vw)`
                 }}
             >
+            <div id={Styles.background_image}><Img fluid={image.image.childImageSharp.fluid} alt="background" /></div>
             <div className={Styles.container}>
                 <div id={Styles.caption}>
                     {image.caption}
                 </div>
                 <div id={Styles.image_container}>
-                    <img src={image.image} alt="slide"/>
+                    <Img fluid={image.image.childImageSharp.fluid} alt="slide"/>
                 </div>
                 <div id={Styles.placeholder}></div>
             </div>
-            <div className={Styles.quote_container}>
-                <p>{image.quote}</p>
-            </div>
         </div>
+        </>
     )
 }
 
