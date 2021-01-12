79128aef12191dd810231d99c66fd9cea3d5e43c
meti651 <meti651@gmail.com>
2021-01-12 23:09:53 +0100

    prepare responsivity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Footer from './footer';
+import Styles from "./layout.module.scss";
 
 import Header from "./header";
 
 const Layout = ({children, onTop}) => {
     return (
-        <>
+        <div id={Styles.main}>
             <Header onTop={onTop}/>
-            {children}
+                {children}
             <Footer/>
-        </>
+        </div>
     )
 }
 
