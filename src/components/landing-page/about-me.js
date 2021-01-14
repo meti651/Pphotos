034420c10a72f3e29e98b2b1ef8ef8b60bfd2698
meti651 <meti651@gmail.com>
2021-01-14 15:08:16 +0100

    about me component responsive

--- src/components/landing-page/about-me.js
+++ src/components/landing-page/about-me.js
@@ -28,20 +28,22 @@ const AboutMe = () =>{
                     Kamaszkorom meghatározó élménye volt mikor kerületi fotópályázatot nyertem, ekkor éreztem, hogy ez az, amivel foglalkozni szeretnék. A gyermeki lelkesedésem azóta és kitart és segít abban, hogy örök és megismételhetetlen pillanatokat varázsoljak neked.
                 </div>
             </div>
-            <div id={Styles.in_image_description_1}>
-                <div>
-                    Különösen kedves számomra mikor egy család életútját ísérhetem végig, ezért
+            <div id={Styles.in_image_descriptions}>
+                <div id={Styles.in_image_description_1}>
+                    <div>
+                        Különösen kedves számomra mikor egy család életútját ísérhetem végig, ezért
+                    </div>
+                    <div className={Styles.strong}>
+                        a kedvenc témáim közé tartozik az esküvő, a kismama fotózás, az újszülött fotózás és a családi képek, portrék megörökítése.
+                    </div>
                 </div>
-                <div className={Styles.strong}>
-                    a kedvenc témáim közé tartozik az esküvő, a kismama fotózás, az újszülött fotózás és a családi képek, portrék megörökítése.
-                </div>
-            </div>
-            <div id={Styles.in_image_description_2}>
-                <div>
-                    A különböző témák sokszínűsége és váltakozása adja meg számomra a szenvedélyt a munkám iránt és számodra a biztonságot, hogy nem befásult tömegszalag fotókat fogsz látni magadról, hanem
-                </div>
-                <div className={Styles.strong}>
-                    élettel teli, valódi, pózmentes képeid lesznek.
+                <div id={Styles.in_image_description_2}>
+                    <div>
+                        A különböző témák sokszínűsége és váltakozása adja meg számomra a szenvedélyt a munkám iránt és számodra a biztonságot, hogy nem befásult tömegszalag fotókat fogsz látni magadról, hanem
+                    </div>
+                    <div className={Styles.strong}>
+                        élettel teli, valódi, pózmentes képeid lesznek.
+                    </div>
                 </div>
             </div>
             <div id={Styles.background_image_container}><Img fluid={backgroundImage.data.childImageSharp.fluid} alt="About me"/></div>
