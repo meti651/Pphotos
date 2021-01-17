407e963cf648975e3c099f8100d05393a7eff1f3
meti651 <meti651@gmail.com>
2021-01-17 12:15:29 +0100

    gallery responsive

--- src/components/gallery/image.js
+++ src/components/gallery/image.js
@@ -8,7 +8,7 @@ const Image = ({image, focusSetter}) => {
 
     const ClickHandler = () => {
         if(container.current.classList.toggle(Styles.focus)){
-            container.current.style.width = `${image.node.childImageSharp.fluid.aspectRatio * 40}vw`;
+            container.current.style.width = `${image.node.childImageSharp.fluid.aspectRatio * 45}%`;
             focusSetter(true);
         }else{
             container.current.style = null;
