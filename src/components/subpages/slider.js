6327600adb6504592ef38733025ac755aef9a40b
meti651 <meti651@gmail.com>
2021-01-18 12:37:30 +0100

    subpages slider responsive base

--- src/components/subpages/slider.js
+++ src/components/subpages/slider.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Arrow from "./arrow";
 import Img from "gatsby-image";
 
@@ -6,6 +6,12 @@ import Styles from "./slider.module.scss";
 
 const Slider = ({images}) => {
     const [translate, setTranslate] = useState(0);
+    const [imageWidth, setImageWidth] = useState();
+
+    useEffect(() => {
+        if(window.innerWidth > 1024)  setImageWidth(290 - (1920 - window.innerWidth)*0.9*0.3);
+        else if(window.innerWidth < 1024 && window.innerWidth > 768) setImageWidth(190);
+    }, [window.innerWidth])
     
     return (
         <div id={Styles.container}>
@@ -14,8 +20,8 @@ const Slider = ({images}) => {
             <div id={Styles.images_container}>
                 {images.map((image,index) => {
                     return(
-                        <div key={index} style={{transform: `translateX(-${30 * translate}vw)`}} className={Styles.image_wrapper}>
-                            <div style={{width: `${image.childImageSharp.fluid.aspectRatio * 370}px`}}>
+                        <div key={index} style={{transform: `translateX(-${100 * translate}%)`}} className={Styles.image_wrapper}>
+                            <div id={Styles.image_container} style={{width: `${image.childImageSharp.fluid.aspectRatio * imageWidth}px`}}>
                                 <Img  fluid={image.childImageSharp.fluid} alt={`${index}`} />
                             </div>
                         </div>
