ea526ff0fb0831b98417ef5433cd31a94cce950a
meti651 <meti651@gmail.com>
2021-01-19 12:21:06 +0100

    family content responsive

--- src/components/subpages/family/content.js
+++ src/components/subpages/family/content.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import Styles from "./content.module.scss";
 
@@ -29,6 +29,21 @@ const Content = () => {
             }
           }
     }`)
+    const [imageWidthProportion, setImageWidthProportion] = useState();
+
+
+    useEffect(() => {
+      const resizeHandler = () => {
+          if(window.innerWidth > 1024)  setImageWidthProportion(window.innerWidth/1920);
+          else if(window.innerWidth <= 1024 && window.innerWidth > 420) setImageWidthProportion(window.innerWidth/1024);
+          else if(window.innerWidth <= 420) setImageWidthProportion(window.innerWidth/768);
+      }
+      
+      window.addEventListener("resize", resizeHandler);
+      resizeHandler();
+      return () => window.removeEventListener("resize", resizeHandler);
+  }, [])
+
     return (
         <div id={Styles.container}>
             <h1>Kedves Édesanyák, Édesapák!</h1>
@@ -36,7 +51,7 @@ const Content = () => {
                 <div className={Styles.paragraph}>Családi fotózásnál két fontos szó határozza meg a képeimet: természetesség és élmények.</div>
                 <div id={Styles.grid1} className={Styles.paragraph}>Gyerkőcökkel fotózni különleges kaland mind a szülőnek mind nekem. Hagynunk kell, hogy a gyerekek kicsit feloldódjanak, és hááát, olyanok legyenek, mint otthon. Aggodalomra semmi ok, garantálhatom, hogy ilyenkor készülnek a legjobb képek… </div>
                 <div className={Styles.image_container} id={Styles.grid2}>
-                    <div className={Styles.blur_border} style={{width: `${images.first.childImageSharp.fluid.aspectRatio * 450}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${images.first.childImageSharp.fluid.aspectRatio * 450 * imageWidthProportion}px`}}>
                         <Img fluid={images.first.childImageSharp.fluid} alt="kid"/>
                     </div>
                 </div>
@@ -44,7 +59,7 @@ const Content = () => {
             <div className={Styles.strong}>Családot fotózni mindig nagy öröm számomra, visszatérő vendégeimnél szuper látni ahogy változnak, növekednek a kicsik.</div>
             <div id={Styles.second_part}>
                 <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${images.second.childImageSharp.fluid.aspectRatio * 700}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${images.second.childImageSharp.fluid.aspectRatio * 700 * imageWidthProportion}px`}}>
                         <Img fluid={images.second.childImageSharp.fluid} alt="kid"/>
                     </div>
                 </div>
@@ -58,8 +73,8 @@ const Content = () => {
                         <li>Elkapott, valódi pillanatokat</li>
                     </ul>
                 </div>
-                <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${images.third.childImageSharp.fluid.aspectRatio * 700}px`}}>
+                <div className={Styles.image_container} id={Styles.responsive}>
+                    <div className={Styles.blur_border} style={{width: `${images.third.childImageSharp.fluid.aspectRatio * 700 * imageWidthProportion}px`}}>
                         <Img fluid={images.third.childImageSharp.fluid} alt="kid"/>
                     </div>
                 </div>
