74ef701d55b2d3629c3e7de4e5112c42c8de3f59
meti651 <meti651@gmail.com>
2021-01-19 21:58:27 +0100

    mum responsive

--- src/components/subpages/mum/content.js
+++ src/components/subpages/mum/content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Styles from "./content.module.scss";
 
@@ -36,6 +36,23 @@ const Content = () => {
             }
           }
     }`)
+
+    const [imageWidthProportion, setImageWidthProportion] = useState();
+
+
+    useEffect(() => {
+      const resizeHandler = () => {
+          if(window.innerWidth > 1024)  setImageWidthProportion(window.innerWidth/1920);
+          else if(window.innerWidth <= 1024 && window.innerWidth > 768) setImageWidthProportion(window.innerWidth/1500);
+          else if(window.innerWidth <= 768 && window.innerWidth > 480) setImageWidthProportion(window.innerWidth/1024);
+          else if(window.innerWidth <= 480) setImageWidthProportion(window.innerWidth/768);
+      }
+      
+      window.addEventListener("resize", resizeHandler);
+      resizeHandler();
+      return () => window.removeEventListener("resize", resizeHandler);
+  }, [])
+
     return (
         <div id={Styles.container}>
             <h1>Kedves Kismamák!</h1>
@@ -43,23 +60,23 @@ const Content = () => {
                 <div className={Styles.text}>
                     Minden nő életének egyik legszebb időszaka ez a bizonyos 9 hónap. Kisbabádat a szíved alatt hordani hatalmas élmény és hidd el, ez a kisugárzásodon is meglátszik
                 </div>
-                <div className={Styles.text}>
-                    Kismamákkal képeket alkotni mindig nagy öröm számomra, hiszen egy olyan ősi erőt érzek bennük, mely semmihez sem fogható. Ekkor teszik meg első lépéseiket az édesanyává válásuk útján és látom rajtuk a szeretetet melyet a még meg nem született gyermekük iránt éreznek.
-                </div>
                 <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio * imageWidthProportion}px`}}>
                         <Img fluid={images.first.childImageSharp.fluid} alt="mum" />
                     </div>
+                </div>
+                <div className={Styles.text}>
+                    Kismamákkal képeket alkotni mindig nagy öröm számomra, hiszen egy olyan ősi erőt érzek bennük, mely semmihez sem fogható. Ekkor teszik meg első lépéseiket az édesanyává válásuk útján és látom rajtuk a szeretetet melyet a még meg nem született gyermekük iránt éreznek.
                 </div>
             </div>
             <div id={Styles.second_part}>
                 <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio * imageWidthProportion}px`}}>
                         <Img fluid={images.second.childImageSharp.fluid} alt="mum" />
                     </div>
                 </div>
                 <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio * imageWidthProportion}px`}}>
                         <Img fluid={images.third.childImageSharp.fluid} alt="mum" />
                     </div>
                 </div>
@@ -82,7 +99,7 @@ const Content = () => {
                     <div>Természetesen! Örömmel várom őt is! De ha nem sikerül rávenni, akkor az is lehet egy kompromisszumos megoldás, ha a fotózás közben egy 10 percre „ugrik csak be”. Ennyi elegendő is pár apás fotó elkészítéséhez is.</div>
                 </div>
                 <div className={Styles.image_container}>
-                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio}px`}}>
+                    <div className={Styles.blur_border} style={{width: `${450 * images.fourth.childImageSharp.fluid.aspectRatio * imageWidthProportion}px`}}>
                         <Img fluid={images.fourth.childImageSharp.fluid} alt="mum" />
                     </div>
                 </div>
