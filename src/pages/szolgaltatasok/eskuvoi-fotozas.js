b54c6e0dced4fa3d30526eb91b7c5cc18b507a18
meti651 <meti651@gmail.com>
2021-10-14 11:32:25 +0200

    Fix images order and format

--- src/pages/szolgaltatasok/eskuvoi-fotozas.js
+++ src/pages/szolgaltatasok/eskuvoi-fotozas.js
@@ -1,82 +1,89 @@
-import { graphql, useStaticQuery } from "gatsby";
-import React from "react";
-import Album from "../../components/landing-page/album";
-import Prices from "../../components/subpages/prices-wedding";
-import SEO from "../../components/seo";
-import { useLocation } from "@reach/router";
+import { graphql, useStaticQuery } from "gatsby"
+import React from "react"
+import Album from "../../components/landing-page/album"
+import Prices from "../../components/subpages/prices-wedding"
+import SEO from "../../components/seo"
+import { useLocation } from "@reach/router"
 
-import SubLayout from "../../components/subpages/subLayout";
-import Content from "../../components/subpages/wedding/content";
-import WeddingBox from "../../components/subpages/wedding/weddingBox";
+import SubLayout from "../../components/subpages/subLayout"
+import Content from "../../components/subpages/wedding/content"
+import WeddingBox from "../../components/subpages/wedding/weddingBox"
 
 const Wedding = () => {
-	const data = useStaticQuery(graphql`
-		{
-			first: file(relativePath: { eq: "slider/wedding/slide1.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			second: file(relativePath: { eq: "slider/wedding/slide2.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			third: file(relativePath: { eq: "slider/wedding/slide3.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			fourth: file(relativePath: { eq: "slider/wedding/slide4.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			fifth: file(relativePath: { eq: "slider/wedding/slide5.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			sixth: file(relativePath: { eq: "slider/wedding/slide6.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    {
+      first: file(relativePath: { eq: "slider/wedding/slide1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      second: file(relativePath: { eq: "slider/wedding/slide2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      third: file(relativePath: { eq: "slider/wedding/slide3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      fourth: file(relativePath: { eq: "slider/wedding/slide4.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      fifth: file(relativePath: { eq: "slider/wedding/slide5.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      sixth: file(relativePath: { eq: "slider/wedding/slide6.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-	const { pathName } = useLocation();
+  const { pathName } = useLocation()
 
-	const pageMetadata = {
-		title: "Esküvői fotózás",
-		description:
-			"Esküvői fotózás. Budapesten és egész Magyarországon belül. Garantáltam a legszebb pillanatok megörökítése a házasságkötésetekről",
-		path: pathName,
-	};
+  const pageMetadata = {
+    title: "Esküvői fotózás",
+    description:
+      "Esküvői fotózás. Budapesten és egész Magyarországon belül. Garantáltam a legszebb pillanatok megörökítése a házasságkötésetekről",
+    path: pathName,
+  }
 
-	const images = [data.second, data.fifth, data.fourth, data.first, data.third, data.sixth];
+  const images = [
+    data.first,
+    data.second,
+    data.third,
+    data.fourth,
+    data.fifth,
+    data.sixth,
+  ]
 
-	return (
-		<SubLayout title={"Esküvői fotózás"} slideImages={images}>
-			<SEO pageMetadata={pageMetadata} />
-			<Content />
-			<Prices />
-			<Album />
-			<WeddingBox />
-		</SubLayout>
-	);
-};
+  return (
+    <SubLayout title={"Esküvői fotózás"} slideImages={images}>
+      <SEO pageMetadata={pageMetadata} />
+      <Content />
+      <Prices />
+      <Album />
+      <WeddingBox />
+    </SubLayout>
+  )
+}
 
-export default Wedding;
+export default Wedding
--- src/pages/szolgaltatasok/portre-fotozas.js
+++ src/pages/szolgaltatasok/portre-fotozas.js
@@ -1,80 +1,87 @@
-import React from "react";
+import React from "react"
 
-import SubLayout from "../../components/subpages/subLayout";
-import Content from "../../components/subpages/portrait/content";
-import Prices from "../../components/subpages/prices-default";
-import Image from "../../components/subpages/portrait/image";
-import SEO from "../../components/seo";
-import { useLocation } from "@reach/router";
+import SubLayout from "../../components/subpages/subLayout"
+import Content from "../../components/subpages/portrait/content"
+import Prices from "../../components/subpages/prices-default"
+import Image from "../../components/subpages/portrait/image"
+import SEO from "../../components/seo"
+import { useLocation } from "@reach/router"
 
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby"
 
 const Portrait = () => {
-	const data = useStaticQuery(graphql`
-		{
-			first: file(relativePath: { eq: "slider/portrait/slide1.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			second: file(relativePath: { eq: "slider/portrait/slide2.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			third: file(relativePath: { eq: "slider/portrait/slide3.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			fourth: file(relativePath: { eq: "slider/portrait/slide4.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			fifth: file(relativePath: { eq: "slider/portrait/slide5.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			sixth: file(relativePath: { eq: "slider/portrait/slide6.jpg" }) {
-				childImageSharp {
-					fluid(maxWidth: 1000, quality: 90) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-		}
-	`);
+  const data = useStaticQuery(graphql`
+    {
+      first: file(relativePath: { eq: "slider/portrait/slide1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      second: file(relativePath: { eq: "slider/portrait/slide2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      third: file(relativePath: { eq: "slider/portrait/slide3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      fourth: file(relativePath: { eq: "slider/portrait/slide4.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      fifth: file(relativePath: { eq: "slider/portrait/slide5.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      sixth: file(relativePath: { eq: "slider/portrait/slide6.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
-	const { pathName } = useLocation();
+  const { pathName } = useLocation()
 
-	const pageMetadata = {
-		title: "Portré fotózás",
-		description:
-			"Portré fotózás. Budapesten és egész Magyarországon belül. Minőségi profilkép Facebookra vagy más közösségi médiára. Modell fotózás.",
-		path: pathName,
-	};
+  const pageMetadata = {
+    title: "Portré fotózás",
+    description:
+      "Portré fotózás. Budapesten és egész Magyarországon belül. Minőségi profilkép Facebookra vagy más közösségi médiára. Modell fotózás.",
+    path: pathName,
+  }
 
-	const images = [data.second, data.first, data.third, data.fifth, data.sixth, data.fourth];
+  const images = [
+    data.first,
+    data.second,
+    data.third,
+    data.fourth,
+    data.fifth,
+    data.sixth,
+  ]
 
-	return (
-		<SubLayout title="Portré fotózás" slideImages={images}>
-			<SEO pageMetadata={pageMetadata} />
-			<Content />
-			<Prices Image={Image} />
-		</SubLayout>
-	);
-};
+  return (
+    <SubLayout title="Portré fotózás" slideImages={images}>
+      <SEO pageMetadata={pageMetadata} />
+      <Content />
+      <Prices Image={Image} />
+    </SubLayout>
+  )
+}
 
-export default Portrait;
+export default Portrait
